f53adea889a7d690ee770d5c2cc8325ed42bcb1a
Arnab Nandi <102936047+arnab7070@users.noreply.github.com>
2024-01-29 11:09:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pickle
 
 app = Flask(__name__)
+CORS(app)
+
+loaded_model = pickle.load(open('model.pkl', 'rb'))
+loaded_vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+def predictHelper(comment):
+    if comment:
+        new_comment = loaded_vectorizer.transform([comment])
+        prediction = loaded_model.predict(new_comment)
+        return True if prediction[0] == 'spam' else False
+    else:
+        return "Internal Server Error"
+
+
+@app.route('/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json()
+    comment = data.get('comment', '')
+    result = predictHelper(comment)
+    return jsonify({'result': result})
+
 
 @app.route('/')
 def index():
